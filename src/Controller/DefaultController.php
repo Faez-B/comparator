2fa7f7783c0ab25a94423d49884412185de0da88
Faez-B <faezbhatti@gmail.com>
2022-08-05 18:07:42 +0200

    Installation de Ignition. Pour enjoliver les pages d'erreurs

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
     {
+        // throw new Exception('Hello Ignition!');
         if ($this->isGranted("ROLE_USER")) {
             return $this->render('default/index.html.twig', []);
         }
