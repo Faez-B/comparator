336802233b54040b3176be21fd8ebcfefb8a4b20
Faez-B <faezbhatti@gmail.com>
2022-08-05 17:22:14 +0200

    Contrôleur pour la route par défaut

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        if ($this->isGranted("ROLE_USER")) {
+            return $this->render('default/index.html.twig', []);
+        }
+        return $this->redirectToRoute("login");
+    }
+}
