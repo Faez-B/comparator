e0158623c4f3ad8dcb5398f6303939ad5eab8eb1
Faez-B <faezbhatti@gmail.com>
2022-08-09 00:09:54 +0200

    Update index Voiture. Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}", 
-    "./templates/**/*.{html,js,twig}"
+    "./templates/**/*.{html,js,twig}",
+    './templates/**/*.html.twig'
   ],
   theme: {
     extend: {},
