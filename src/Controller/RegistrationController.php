76cc9f6a2a676a9779611f9e6f1ca6f1892b131e
Faez-B <faezbhatti@gmail.com>
2022-11-13 19:12:48 +0100

    Mail sent after new account registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,18 +4,21 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -30,9 +33,27 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            $user->setRoles(["ROLE_USER"]);
+
             $entityManager->persist($user);
-            $entityManager->flush();
+
             // do anything else you need here, like send an email
+            $message = (new TemplatedEmail())
+                        ->subject("Création d'un nouveau compte")
+                        ->from("from@test.com")
+                        ->to(new Address("destinataire1@test.com"))
+                        ->addTo(new Address("destinataire2@test.com"))
+                        ->addTo(new Address("destinataire3@test.com"))
+                        ->htmlTemplate("emails/register.html.twig")
+                        ->context([
+                            'user' => $user,
+                            "mdp" => $form->get('plainPassword')->getData()
+                        ])
+            ;
+
+            $mailer->send($message);
+
+            $entityManager->flush();
 
             return $this->redirectToRoute('index');
         }
